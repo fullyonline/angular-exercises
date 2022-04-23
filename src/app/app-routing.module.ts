cbe0661e763978f6ac3e3d50553659f2c0864731
Romano Sabbatella <romano_sabbi_1993@hotmail.com>
2022-04-23 12:38:54 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ParentTextComponent} from "./components/parent-text/parent-text.component";
+import {JokeServiceComponent} from "./components/joke-service/joke-service.component";
+import {PipeExampleComponent} from "./components/pipe-example/pipe-example.component";
+import {AppComponent} from "./components/app/app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'parent', component: ParentTextComponent },
+  { path: 'joke', component: JokeServiceComponent },
+  { path: 'pipe', component: PipeExampleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
